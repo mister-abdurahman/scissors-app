8f4da3e76d6461ab82d67a31bb7ab473ee6851d4
abdurahman <aramramadan7@gmail.com>
2023-06-08 22:02:28 +0100

    navbar

---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+export function NavBar(){
+    return <nav className="nav-bar">
+        <span className="logo"></span>
+        <ul className="nav-items">
+            <li>Why Scissors?</li>
+            <li>Products</li>
+            <li>Resources</li>
+            <li>Sign up free</li>
+            <li>Log in</li>
+            </ul>
+    </nav>
+}
