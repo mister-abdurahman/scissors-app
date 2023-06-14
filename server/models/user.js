ccfddd10d5e8d1344fa9c2379b6499c480214163
abdurahman <aramramadan7@gmail.com>
2023-06-14 22:15:53 +0100

    user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,1 +1,24 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+// const bcrypt = require("bcrypt");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: [true, "Enter your Email"], unique: true },
+    password: {
+      type: String,
+      minLength: 4,
+      required: [
+        true,
+        "Your password should contain lower and upper letter case, a number, a character and be at least 5 length long",
+      ],
+    },
+    urlId: { type: Array, default: [] },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('user', userSchema);
